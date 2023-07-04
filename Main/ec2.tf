6a557acafd9d1e0006476119294184e2d06927aa
M03HE <moshemhd@gmail.com>
2023-07-04 13:46:03 +0300

    NGINX WORKING

--- Main/ec2.tf
+++ Main/ec2.tf
@@ -1,45 +1,39 @@
+locals {
+  private_key_path = "~/Downloads/devops.pem"
+  key_name         = "devops"
+}
+resource "local_file" "ssh_key" {
+  filename = "${local.key_name}.pem"
+  content = local.private_key_path
+  file_permission = "0400"
+}
 resource "aws_instance" "web1" {
-  ami           = lookup(var.AMI, var.AWS_REGION)
-  instance_type = var.ec2_instance_type
-  subnet_id     = aws_subnet.prod-subnet-public-1.id
-  key_name      = aws_key_pair.my-key-pair.key_name
-  # vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
-  security_groups             = [aws_security_group.ssh-allowed.id]
+  # ami                         = lookup(var.AMI, var.AWS_REGION)
+  ami                         = "ami-01dd271720c1ba44f"
+  subnet_id                   = aws_subnet.prod-subnet-public-1.id
+  instance_type               = var.ec2_instance_type
   associate_public_ip_address = true
+  # security_groups             = [aws_security_group.ssh-allowed.id]
+  key_name                    = local.key_name
+  # subnet_id                   = "subnet-01988185fe9c14e04"
+  # instance_type               = "t2.micro"
+  # ami                    = "ami-01dd271720c1ba44f"
+  vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
 
   provisioner "remote-exec" {
-    inline = ["echo 'wait untill SSH is ready"]
-
+    inline = ["echo 'Wait untill SSH is ready'"]
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("${path.module}/my-key-pair")
-      host        = aws_instance.web1.public_ip
+      private_key = file(local_file.ssh_key.content)
+      host        = self.public_ip
     }
   }
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.web1.public_ip}, --private-key ${file("${path.module}/my-key-pair")} nginx.yaml"
+    command = "ansible-playbook -i ${aws_instance.web1.public_ip}, --private-key ${local_file.ssh_key.content} nginx.yaml"
   }
 }
 
-locals {
-  private_key_path = file("${path.module}/my-key-pair")
+output "nginx_ip" {
+  value = aws_instance.web1.public_ip
 }
-# Nginx installation
-# provisioner "file" {
-#   source      = "nginx.sh"
-#   destination = "/tmp/nginx.sh"
-# }
-# provisioner "remote-exec" {
-#   inline = [
-#     "chmod +x /tmp/nginx.sh",
-#     "sudo /tmp/nginx.sh"
-#   ]
-# }
-
-#   connection {
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     private_key = file("${path.module}/London-region-key-pair")
-#     host        = self.public_ip
-#   }
--- Main/network.tf
+++ Main/network.tf
@@ -24,32 +24,31 @@ resource "aws_route_table_association" "prod-rta-public-subnet-1" {
   route_table_id = aws_route_table.prod-public-rt.id
 }
 
-resource "aws_key_pair" "my-key-pair" {
-  key_name   = "my-key-pair"
-  public_key = file("${path.module}/my-key-pair.pub")
-}
+# resource "aws_key_pair" "my-key-pair" {
+  # key_name   = "my-key-pair"
+  # public_key = local.private_key_path
+  # public_key = file("${path.module}/my-key-pair.pub")
+# }
 
 resource "aws_security_group" "ssh-allowed" {
+  name = "nginx_access"
   vpc_id = aws_vpc.prod-vpc.id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    //this means all ip adderss are allowed to ssh ! 
-    // do not do this in prodoction!
     cidr_blocks = ["0.0.0.0/0"]
   }
-  //if you do not add this rule, you can not reach the NGIX
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
