c848411096f0d1adf981d3a86998ce567b9d2c7a
M03HE <moshemhd@gmail.com>
2023-07-04 16:33:47 +0300

    ansible playbook docker install working

--- Main/ec2.tf
+++ Main/ec2.tf
@@ -8,17 +8,14 @@ resource "local_file" "ssh_key" {
   file_permission = "0400"
 }
 resource "aws_instance" "web1" {
-  # ami                         = lookup(var.AMI, var.AWS_REGION)
-  ami                         = "ami-01dd271720c1ba44f"
+  ami                         = lookup(var.AMI, var.AWS_REGION)
   subnet_id                   = aws_subnet.prod-subnet-public-1.id
   instance_type               = var.ec2_instance_type
   associate_public_ip_address = true
-  # security_groups             = [aws_security_group.ssh-allowed.id]
   key_name                    = local.key_name
-  # subnet_id                   = "subnet-01988185fe9c14e04"
-  # instance_type               = "t2.micro"
-  # ami                    = "ami-01dd271720c1ba44f"
   vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
+  # security_groups             = [aws_security_group.ssh-allowed.id]
+
 
   provisioner "remote-exec" {
     inline = ["echo 'Wait untill SSH is ready'"]
@@ -30,7 +27,7 @@ resource "aws_instance" "web1" {
     }
   }
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.web1.public_ip}, --private-key ${local_file.ssh_key.content} nginx.yaml"
+    command = "ansible-playbook -i ${aws_instance.web1.public_ip}, --private-key ${local_file.ssh_key.content} jenkins.yaml"
   }
 }
 
