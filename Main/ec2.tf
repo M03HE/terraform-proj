db33d3238840a7c94bdd69d5cb38842deebc41b9
M03HE <moshemhd@gmail.com>
2023-07-05 10:32:02 +0300

    Key Pair EC2 AUTOMATIC CREATRED VIA TERRAFORM

--- Main/ec2.tf
+++ Main/ec2.tf
@@ -1,11 +1,21 @@
 locals {
-  private_key_path = "~/Downloads/devops.pem"
-  key_name         = "devops"
+  private_key_path = "./devopss.pem"
+  key_name         = "devopss"
+}
+
+resource "tls_private_key" "demo_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_pair_name
+  public_key = tls_private_key.demo_key.public_key_openssh
 }
 
 resource "local_file" "ssh_key" {
   filename        = "${local.key_name}.pem"
-  content         = local.private_key_path
+  content         = tls_private_key.demo_key.private_key_pem
   file_permission = "0400"
 }
 
@@ -22,12 +32,12 @@ resource "aws_instance" "web1" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file(local_file.ssh_key.content)
+      private_key = local_file.ssh_key.content
       host        = self.public_ip
     }
   }
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${self.public_ip}, --private-key ${local_file.ssh_key.content} jenkins.yaml"
+    command = "ansible-playbook -i ${self.public_ip}, --private-key ${local.private_key_path} jenkins.yaml"
   }
 }
 
--- Main/variables.tf
+++ Main/variables.tf
@@ -21,3 +21,9 @@ variable "azs" {
   default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 }
 
+variable "key_pair_name" {
+  description = "The name of the ec2 key pair"
+  type        = string
+  default     = "devopss"
+}
+
